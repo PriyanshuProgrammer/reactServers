0ba2c65745e644442431f4b53e1f92e97db9a796
priyanshuProgrammer <priyanshu22600@gmail.com>
2025-04-13 01:34:29 +0530

    async comps

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,12 @@
-export const Card = () => {
+export const Card = async () => {
+  const data = await fetch('https://api.adviceslip.com/advice');
+  const res = await data.json();
+  const { slip } = res;
   return (
     <div>
       <h1>Random Number</h1>
+      <h2>{slip.id}</h2>
+      <p>{slip.advice}</p>
     </div>
   )
 }
