88a8833447e8d86e63bac5deb9b02f939e2b449d
priyanshuProgrammer <priyanshu22600@gmail.com>
2025-04-13 20:26:10 +0530

    change 2

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,10 @@
+'use server';
 export const Card = async () => {
   const data = await fetch('https://api.adviceslip.com/advice');
   const res = await data.json();
   const { slip } = res;
   console.log("card comp")
+  console.log(slip)
   return (
     <div>
       <h1>this is async comp</h1>
